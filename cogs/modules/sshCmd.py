ea84d8f8ebaf7c85992888cc6347373974c8b041
Wayde <git@alphaxdev.xyz>
2021-06-15 14:54:59 +1000

    error fix soontm
    soontm

--- cogs/modules/sshCmd.py
+++ cogs/modules/sshCmd.py
@@ -19,5 +19,5 @@ async def run(ctx, messageb, user):
         embed=discord.Embed(title="Gravity Output", description=f'```{stderr.decode()[-250:]}\n\n```', colour=0xFF0000)
         embed.add_field(name="** **", value=f"```Time Started: {start_time.replace(microsecond=0)}\nTime Ended: {datetime.now().replace(microsecond=0)}\nTime Taken: {datetime.now().replace(microsecond=0) - startTime.replace(microsecond=0)}```", inline=False)
         await messageb.edit(embed=embed)
-        await messageb.reply(f'<@{user}> done!', mention_author=True)
+        await messageb.reply(f'<@{user}> error!', mention_author=True)
     await p.wait()
